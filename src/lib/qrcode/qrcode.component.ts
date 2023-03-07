188a6e131781ed74dd7107e6cbf4ab43aeabbc8f
AlexStorm <info@alexstorm.ru>
2023-03-07 10:53:03 +0530

    extract code to fl-crypto and ui-qr

---
+++ src/lib/qrcode/qrcode.component.ts
@@ -0,0 +1,30 @@
+import {Component, ElementRef, Input, ViewChild} from '@angular/core';
+import * as qrcode from 'qrcode';
+
+@Component({
+  selector: 'ui-qrcode',
+  templateUrl: './qrcode.component.html',
+  styleUrls: ['./qrcode.component.scss']
+})
+export class QrcodeComponent {
+  @ViewChild('qrCodeCanvas', { static: true }) qrCodeCanvas: ElementRef;
+
+  @Input('data')
+  set setData(data: string) {
+
+
+    // Generate the QR code as a data URI
+    qrcode.toDataURL(data)
+        .then(url => {
+          // Render the QR code on the canvas
+          const canvas = this.qrCodeCanvas.nativeElement;
+          const context = canvas.getContext('2d');
+          const img = new Image();
+          img.onload = () => {
+            context.drawImage(img, 0, 0);
+          };
+          img.src = url;
+        })
+        .catch(err => console.error(err));
+  }
+}
